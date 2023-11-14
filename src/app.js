0d2f9d6ea1cbc3c47247e62ebf3b5d004ea812ff
henrique ferraz <henriqueferraz@gmail.com>
2023-11-13 21:21:28 -0300

    commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo');
+});
+
+app.get('/user/:nome', (req, res) => {
+  res.send({
+    nome: req.params.nome,
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Servidor rodando');
+});
